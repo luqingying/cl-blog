6bde4855cfdc87d41a6f92959e61ab823bbcf7ee
chen <im.cchen@foxmail.com>
2019-02-20 15:40:46 +0800

    :hammer: 提交部分Ajax请求代码

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,27 +1,26 @@
-
 new Vue({
     el: '.small-nav',
     data: {
         isShow: false
     }
-})
+});
 
 new Vue({
     el: '.pull-down',
     data: {
         isShow: false
     },
-    methods:{
-        onEnter:function (isShow) {
+    methods: {
+        onEnter: function (isShow) {
             isShow = !isShow;
             return isShow;
         },
-        onOut:function (isShow) {
+        onOut: function (isShow) {
             isShow = !isShow;
             return isShow;
         }
     }
-})
+});
 
 /**
  * Created by luqingying on 2019/1/23.
